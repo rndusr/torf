d86ee290feee3d04e1dea46e7219a722541f7e37
Random User <rndusr@posteo.de>
2017-12-23 16:31:30 +0100

    Remove mkdoc.sh script

--- mkdoc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pydoc3 torf.Torrent | \
-    tail -n +6 | \
-    sed 's/^..../   /' | \
-    sed 's/^\s*$//' | \
-    sed '/__repr__/,/^$/ d' | \
-    sed '/__dict__/,/^$/ d' | \
-    sed '/__weakref__/,/^$/ d'
