692e762fa665719e073d80302203e90d41658bf7
Random User <rndusr@posteo.de>
2019-10-21 17:13:12 +0200

    Add exception ContentError

--- torf/_errors.py
+++ torf/_errors.py
@@ -96,6 +96,48 @@ class FileSizeError(TorfError):
                          f'{actual_size} instead of {expected_size} bytes')
 
 
+class ContentError(TorfError):
+    """On-disk data does not match hashes in metainfo"""
+    def __init__(self, piece_index, piece_size, files):
+        self._errno = errno.EIO
+        msg = f'Unexpected bytes in piece {piece_index+1}'
+
+        if len(files) > 1:
+            # Find the slice in the whole stream of files that contains the
+            # corruption (piece_index=0 is the first piece)
+            err_i_beg = piece_index * piece_size
+            err_i_end = err_i_beg + piece_size
+
+            # Find the files that are covered by the corrupt piece
+            corrupt_files = []
+            cur_pos = 0
+            for i,file in enumerate(files):
+                filesize = os.path.getsize(file)
+                file_i_beg = cur_pos
+                file_i_end = file_i_beg + filesize
+
+                # `file` is possibly corrupt if:
+                # 1. The corrupt piece STARTS between the beginning and the end
+                #    of the file in the stream.
+                # 2. The corrupt piece ENDS between the beginning and the end
+                #    of the file in the stream.
+                # 3. Both beginning and end of the file are between beginning
+                #    and end of the corrupt piece (i.e. file fits in one piece).
+                if (file_i_beg <= err_i_beg < file_i_end or
+                    file_i_beg < err_i_end <= file_i_end or
+                    (file_i_beg >= err_i_beg and file_i_end < err_i_end)):
+                    corrupt_files.append(files[i])
+                cur_pos += filesize
+
+            if len(corrupt_files) == 1:
+                msg += f' in {corrupt_files[0]}'
+            else:
+                msg += (', at least one of these files is corrupt: ' +
+                        ', '.join(corrupt_files))
+
+        super().__init__(msg)
+
+
 class ReadError(TorfError):
     """Unreadable file or stream"""
     def __init__(self, error_code, path=None):
