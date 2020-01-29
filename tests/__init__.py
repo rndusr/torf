9c0ed8cd21139dbceafc5fc5d9c88eee120b5edd
Random User <rndusr@posteo.de>
2020-01-29 15:14:37 +0100

    display_filespecs(): Ignore line prefix for proper header alignment

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,8 +6,7 @@ def display_filespecs(filespecs, piece_size):
     header = [' ' * (((4*filecount) + (2*filecount-1)) + 2)]
     for i in range(6):
         header.append(str(i) + ' '*(piece_size-1))
-    debug(''.join(header))
     line = (', '.join(f'{fn}:{fs:2d}' for fn,fs in filespecs),
             ' - ',
             ''.join(fn*fs for fn,fs in filespecs))
-    debug(''.join(line))
+    debug(f'\n{"".join(header)}\n{"".join(line)}')
