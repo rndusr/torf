4d464452b70fe7cc1162df3f60853d1bf4be4b9c
Random User <rndusr@posteo.de>
2020-02-14 19:20:14 +0100

    Do not limit faked trailing_bytes to faked file size
    We NEED that exact number of trailing_bytes, even if the faked file is smaller
    than remaining_bytes, i.e. there are three or more tiny files in the same piece
    and we're carrying over bytes from a previous file.

--- torf/_generate.py
+++ torf/_generate.py
@@ -511,10 +511,9 @@ class _FileFaker():
             # Read trailing_bytes from the end of `filepath`.  Even if
             # `filepath` is corrupt, its final bytes may be fine and the first
             # piece of the next file can be saved.
-            seek_pos = min(remaining_bytes, self._file_sizes[filepath])
-            _debug(f'reader: Seeking to {-seek_pos} in {os.path.basename(filepath)}')
+            _debug(f'reader: Seeking to {-remaining_bytes} in {os.path.basename(filepath)}')
             with open(filepath, 'rb') as f:
-                f.seek(-seek_pos, os.SEEK_END)
+                f.seek(-remaining_bytes, os.SEEK_END)
                 trailing_bytes = f.read(remaining_bytes)
             _debug(f'reader: Read {len(trailing_bytes)} trailing bytes '
                    f'from {os.path.basename(filepath)}: {_pretty_bytes(trailing_bytes)}')
