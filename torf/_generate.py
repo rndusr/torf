8df1da07f430d43817c679db84db0e7e3290e4c1
Random User <rndusr@posteo.de>
2020-02-14 19:19:36 +0100

    Fix piece_index of VerifyFileSizeError

--- torf/_generate.py
+++ torf/_generate.py
@@ -199,11 +199,12 @@ class Reader():
                     exc = error.VerifyFileSizeError(filepath,
                                                     actual_size=filesize,
                                                     expected_size=spec_filesize)
-                    piece_index = self._calc_piece_index()
+                    # Get index of the next byte we would chunk
+                    piece_index = self._calc_piece_index(1)
                     self._push(piece_index, None, filepath, exc)
                     # No need to read this file
                     self.skip_file(filepath, piece_index, force=True)
-                    self._dont_skip_piece(piece_index+1)
+                    self._dont_skip_piece(piece_index + 1)
                     return False
         return True
 
