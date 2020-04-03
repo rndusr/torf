329f8f7f4f096d5eaba1421ba669fb987731122b
Random User <rndusr@posteo.de>
2020-04-03 10:02:10 +0200

    setup.py: Remove specific Python versions

--- setup.py
+++ setup.py
@@ -34,9 +34,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries',
     ]
 )
