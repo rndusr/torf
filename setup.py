45f23829c6b1dac1602b072ef41af923973ac645
Random User <rndusr@posteo.de>
2020-04-07 14:55:25 +0200

    Indicate support for 3.6/7/8 only

--- setup.py
+++ setup.py
@@ -33,7 +33,9 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Software Development :: Libraries',
     ]
 )
