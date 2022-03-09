e6dd96ad7ba7dd5a60c040ffab3431a8ae663bad
Random User <rndusr@posteo.de>
2022-03-09 12:09:31 +0100

    Drop support for Python 3.6, indicate support for 3.9/10

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     version            = __version__,
     license            = 'GPLv3+',
     packages           = find_packages(exclude=['tests']),
-    python_requires    = '>=3.6, ==3.*',
+    python_requires    = '>=3.7, ==3.*',
     install_requires   = ['flatbencode==0.2.*'],
 
     author             = 'Random User',
@@ -33,9 +33,10 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Software Development :: Libraries',
     ]
 )
