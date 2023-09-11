rndusr/torf

commit 25fb2f888c91256cfb0fc374ef9938f9e683ba0a

Decode bencoded values as UTF-8, replace illegal characters

torf/_utils.py
@@ -730,12 +730,9 @@ def assert_type(obj, keys, exp_types, must_exist=True, check=None):
 
 def decode_value(value):
     if isinstance(value, collections.abc.ByteString):
-        # Torrents can contain binary data in non-standard fields. There is no
-        # way to tell if a field is supposed to be decoded as a string or not.
-        try:
-            return bytes.decode(value, encoding='utf-8', errors='strict')
-        except UnicodeDecodeError:
-            return value
+        # WARNING: Torrents can contain binary data (e.g. "pieces" field). You
+        #          should handle and remove those separately beforehand.
+        return bytes.decode(value, encoding='utf-8', errors='replace')
     elif isinstance(value, collections.abc.Sequence):
         return decode_list(value)
     elif isinstance(value, collections.abc.Mapping):

commit 9c4782fb34f41cf759d483096f0abbcb81640b49

Preserve non-UTF8 encoded metainfo (fixes #31)
This bug was introduced by 25fb2f888c91256cfb0fc374ef9938f9e683ba0a.

Instead of decoding as UTF-8 and replacing illegal characters in all the
metainfo, we are now doing that only for known string-values (name, files,
etc).

torf/_torrent.py
@@ -217,12 +217,25 @@ class Torrent():
         """
         info = self.metainfo['info']
         if self.mode == 'singlefile':
-            files = (utils.File(info.get('name', DEFAULT_TORRENT_NAME), size=self.size),)
+            files = (
+                utils.File(
+                    utils.force_as_string(
+                        info.get('name', DEFAULT_TORRENT_NAME)
+                    ),
+                    size=self.size,
+                ),
+            )
         elif self.mode == 'multifile':
-            basedir = info.get('name', DEFAULT_TORRENT_NAME)
-            files = (utils.File(os.path.join(basedir, *fileinfo['path']),
-                                size=fileinfo['length'])
-                     for fileinfo in info['files'])
+            basedir = utils.force_as_string(
+                info.get('name', DEFAULT_TORRENT_NAME)
+            )
+            files = (
+                utils.File(
+                    os.path.join(basedir, *(utils.force_as_string(p) for p in fileinfo['path'])),
+                    size=fileinfo['length'],
+                )
+                for fileinfo in info['files']
+            )
         else:
             files = ()
         return utils.Files(files, callback=self._files_changed)
@@ -523,7 +536,9 @@ class Torrent():
         """
         if 'name' not in self.metainfo['info'] and self.path is not None:
             self.metainfo['info']['name'] = self.path.name
-        return self.metainfo['info'].get('name', None)
+        return utils.force_as_string(
+            self.metainfo['info'].get('name', None)
+        )
 
     @name.setter
     def name(self, value):
@@ -922,7 +937,9 @@ class Torrent():
 
         Setting this property sets or removes :attr:`metainfo`\\ ``['comment']``.
         """
-        return self.metainfo.get('comment', None)
+        return utils.force_as_string(
+            self.metainfo.get('comment', None)
+        )
 
     @comment.setter
     def comment(self, value):
@@ -966,7 +983,9 @@ class Torrent():
         Setting this property sets or removes
         :attr:`metainfo`\\ ``['created by']``.
         """
-        return self.metainfo.get('created by', None)
+        return utils.force_as_string(
+            self.metainfo.get('created by', None)
+        )
 
     @created_by.setter
     def created_by(self, value):
@@ -983,7 +1002,9 @@ class Torrent():
         Setting this property sets or removes
         :attr:`metainfo`\\ ``['info']``\\ ``['created by']``.
         """
-        return self.metainfo['info'].get('source', None)
+        return utils.force_as_string(
+            self.metainfo['info'].get('source', None)
+        )
 
     @source.setter
     def source(self, value):
@@ -1358,7 +1379,7 @@ class Torrent():
 
         # Check values shared by singlefile and multifile torrents
         utils.assert_type(md, ('info',), (dict,), must_exist=True)
-        utils.assert_type(md, ('info', 'name'), (str,), must_exist=True)
+        utils.assert_type(md, ('info', 'name'), (str, bytes), must_exist=True)
         utils.assert_type(md, ('info', 'piece length'), (int,), must_exist=True,
                           check=utils.is_divisible_by_16_kib)
         utils.assert_type(md, ('info', 'pieces'), (abc.ByteString,), must_exist=True)
@@ -1410,7 +1431,7 @@ class Torrent():
                 utils.assert_type(md, ('info', 'files', i, 'path'), (utils.Iterable,), must_exist=True)
                 utils.assert_type(md, ('info', 'files', i, 'md5sum'), (str,), must_exist=False, check=utils.is_md5sum)
                 for j,item in enumerate(fileinfo['path']):
-                    utils.assert_type(md, ('info', 'files', i, 'path', j), (str,))
+                    utils.assert_type(md, ('info', 'files', i, 'path', j), (str, bytes))
 
             # - validate() should ensure that ['info']['pieces'] is math.ceil(self.size /
             #   self.piece_size) bytes long.

torf/_utils.py
@@ -727,11 +727,33 @@ def assert_type(obj, keys, exp_types, must_exist=True, check=None):
         raise error.MetainfoError(f"{keychain_str}[{key!r}] is invalid: {obj[key]!r}")
 
 
+def force_as_string(value):
+    """
+    Return `value` as string, replace non-UTF8 characters with "�"
+    """
+    if isinstance(value, bytes):
+        return value.decode('utf8', errors='replace')
+    elif isinstance(value, str) or value is None:
+        return value
+    else:
+        return str(value)
+
+
 def decode_value(value):
     if isinstance(value, collections.abc.ByteString):
-        # WARNING: Torrents can contain binary data (e.g. "pieces" field). You
-        #          should handle and remove those separately beforehand.
-        return bytes.decode(value, encoding='utf-8', errors='replace')
+        # Try to decode `value` as UTF8, but return it as-is if that fails
+        # because we don't want to change the infohash. Non-UTF8-encoded strings
+        # (torrent name, files, etc) should be stored as bytes and decoded on
+        # demand by the relevant Torrent attributes.
+        #
+        # TODO: We should store the metainfo as bytes so it can be used to
+        #       create a byte-for-byte identical torrent, regardless of encoding
+        #       or weird stuff stored in the original torrent. But that's a lot
+        #       of work.
+        try:
+            return bytes.decode(value, encoding='utf8', errors='strict')
+        except UnicodeDecodeError:
+            return value
     elif isinstance(value, collections.abc.Sequence):
         return decode_list(value)
     elif isinstance(value, collections.abc.Mapping):

tests/test_read.py
@@ -8,6 +8,7 @@ import flatbencode as bencode
 import pytest
 
 import torf
+from torf import _utils
 
 
 def test_non_bencoded_data():
@@ -220,3 +221,32 @@ def test_read_from_torrent_file_with_empty_path_components(valid_multifile_metai
     t = torf.Torrent.read(str(f))
     exp_path = f'{valid_multifile_metainfo[b"info"][b"name"].decode()}/foo/bar'
     assert exp_path in tuple(str(f) for f in t.files)
+
+
+def test_read_nonutf8_encoded_metainfo(valid_multifile_metainfo, tmp_path):
+    valid_multifile_metainfo[b'comment'] = 'A çommقnt'.encode('CP1256')
+    valid_multifile_metainfo[b'created by'] = 'bДd ЗncodeЯ'.encode('CP866')
+    valid_multifile_metainfo[b'info'][b'name'] = 'Thê ñãme'.encode('CP860')
+    valid_multifile_metainfo[b'info'][b'source'] = 'Þhe ßource'.encode('CP861')
+    valid_multifile_metainfo[b'info'][b'files'] = [
+        {
+            b'path': [
+                'FΩO'.encode('ISO8859-7'),
+                'BAΓ'.encode('ISO8859-7'),
+                'βAZ'.encode('ISO8859-7'),
+            ],
+            b'length': 124,
+        },
+    ]
+
+    f = (tmp_path / 'test.torrent')
+    f.write_bytes(bencode.encode(valid_multifile_metainfo))
+
+    t = torf.Torrent.read(str(f))
+    assert t.name == 'Th� ��me'
+    assert t.comment == 'A �omm�nt'
+    assert t.created_by == 'b�d �ncode�'
+    assert t.source == '�he �ource'
+    assert t.files == [
+        _utils.File('Th� ��me/F�O/BA�/�AZ', size=124),
+    ]

tests/test_utils.py
@@ -262,7 +262,7 @@ def test_decoding():
     assert utils.decode_dict(encoded) == decoded
 
 def test_decoding_invalid_unicode():
-    assert utils.decode_value(b'\xed') == '\uFFFD'  # REPLACEMENT CHARACTER: �'
+    assert utils.decode_value(b'\xed') == b'\xed'
 
 
 def test_encoding():

tests/test_validate.py
@@ -28,7 +28,7 @@ def test_wrong_name_type(generated_singlefile_torrent):
     with pytest.raises(torf.MetainfoError) as excinfo:
         t.validate()
     assert str(excinfo.value) == ("Invalid metainfo: ['info']['name'] "
-                                  "must be str, not int: 123")
+                                  "must be str or bytes, not int: 123")
 
 def test_wrong_piece_length_type(generated_singlefile_torrent):
     t = generated_singlefile_torrent
@@ -276,7 +276,7 @@ def test_multifile_wrong_path_item_type(generated_multifile_torrent):
     with pytest.raises(torf.MetainfoError) as excinfo:
         t.validate()
     assert str(excinfo.value) == ("Invalid metainfo: ['info']['files'][1]['path'][0] "
-                                  "must be str, not int: 17")
+                                  "must be str or bytes, not int: 17")
 
 def test_multifile_wrong_length_type(generated_multifile_torrent):
     t = generated_multifile_torrent